d9ee9ee842c599b0d73e62c54dc0c32d4b9cb5cf
mayra_nggra <118453725+nggra@users.noreply.github.com>
2025-05-27 15:01:22 +0700

    Add files via upload

--- project_Streamlit_uas_bengkod.py
+++ project_Streamlit_uas_bengkod.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1qvuxgGLBfmpSD8XMlTHHL2rRHFjJS4gl
 """
 
-!pip install streamlit
+#!pip install streamlit
 
 import streamlit as st
 import joblib
