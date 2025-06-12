8c13c18868e64da623bc30865aa06a75ee9b6923
mayra_nggra <118453725+nggra@users.noreply.github.com>
2025-06-12 14:43:39 +0700

    Update project_Streamlit_uas_bengkod.py

--- project_Streamlit_uas_bengkod.py
+++ project_Streamlit_uas_bengkod.py
@@ -137,6 +137,8 @@ df_clean[numeric_columns] = scaler.fit_transform(df_clean[numeric_columns])
 # Save the scaler
 joblib.dump(scaler, "scaler.pkl")
 joblib.dump(encoder, "label_encoder.pkl")
+joblib.dump(best_rf, "random_forest_model.pkl")
+
 
 
 # Pisahkan fitur dan target
